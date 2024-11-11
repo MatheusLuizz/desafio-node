780bbe3adf4e496709711a2ed293debf41f91092
unknown <matheus.luizf35@gmail.com>
2024-11-11 19:09:15 -0300

    Autenticação com JWT

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,10 +3,12 @@ const jwt = require('jsonwebtoken');
 const authenticateToken = (req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
 
+  // verificando se o token foi fornecido
   if (!token) {
     return res.status(401).json({ error: 'Acesso negado! Token não fornecido.' });
   }
 
+  // se o token foi fornecido, verificará se ele é válido
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
       return res.status(403).json({ error: 'Token inválido.' });
